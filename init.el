0e69b367e6feb1a4746d03bf585d3207fe79a7b2
Prakash <prakraja@umail.iu.edu>
2015-10-26 01:13:55 -0400

    Adding yaml and jedi setup

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 (progn
   (setq package-list '(flx-ido tern js2-mode sr-speedbar tabbar fiplr magit multiple-cursors helm tern multiple-cursors
 			       yasnippet auto-complete helm-gtags markdown-mode web-mode move-text web-beautify jedi python-mode
-			       ac-js2 tern-auto-complete))
+			       ac-js2 tern-auto-complete yaml-mode))
   ;; Now putting some autoInstall stuff
   (setq package-archives '(
 			   ("gnu" . "http://elpa.gnu.org/packages/")
@@ -196,5 +196,14 @@
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (setq py-electric-colon-active t)
+(progn
+  (require 'jedi)
+  (add-hook 'python-mode-hook
+	  (lambda ()
+	    (jedi:setup)
+	    (jedi:ac-setup)
+            (local-set-key "\C-cd" 'jedi:show-doc)
+            (local-set-key (kbd "M-SPC") 'jedi:complete)
+            (local-set-key (kbd "M-.") 'jedi:goto-definition))))
 ;; (add-hook 'python-mode-hook 'autopair-mode)(add-hook 'python-mode-hook 'auto-complete-mode)
 ;; ******************* End Python suff ***********************************
