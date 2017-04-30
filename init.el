8ff48555903dc6efcd63ae2a29f60e85352b8b7a
Prakash <prak5190@gmail.com>
2017-04-30 13:43:55 -0700

    Disabling some packages which are causing errors

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 
 ;; list the packages you want
 (progn
-  (setq package-list '(flx-ido tern js2-mode sr-speedbar tabbar fiplr magit multiple-cursors helm tern multiple-cursors
+  (setq package-list '(flx-ido tern js2-mode sr-speedbar fiplr magit multiple-cursors helm tern multiple-cursors
 			       yasnippet auto-complete helm-gtags markdown-mode web-mode move-text web-beautify jedi python-mode transpose-frame vlf auto-compile
 			       ;; Taken from https://github.com/tuhdo/emacs-c-ide-demo/blob/master/init.el
 			       anzu duplicate-thing ggtags helm-gtags helm-projectile helm-swoop flycheck function-args merlin
@@ -38,11 +38,12 @@
     (unless (package-installed-p package)
       (package-install package))))
 
-(setq load-prefer-newer t)
-(require 'auto-compile)
-(auto-compile-on-load-mode)
-(auto-compile-on-save-mode)
-(setq exec-path (append exec-path '("/home/el/scala/bin")))
+(progn
+  (setq load-prefer-newer t)
+  (require 'auto-compile)
+  (auto-compile-on-load-mode)
+  (auto-compile-on-save-mode)
+  (setq exec-path (append exec-path '("/home/el/scala/bin"))))
 ;; **** Custom Vars- Theme and stuff ***********
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -82,23 +83,26 @@
  '(web-mode-script-padding 2))
 (require 'vlf-setup)
 ;; ;********************** Flx IDO stuff ********************8
-(require 'flx-ido)
-(ido-mode 1)
-(ido-everywhere 1)
-;(flx-ido-mode 1)
-;; disable ido faces to see flx highlights.
-(setq ido-enable-flex-matching t)
-(setq ido-use-faces nil)
-(setq gc-cons-threshold 20000000)
+(progn
+  (require 'flx-ido)
+  (ido-mode 1)
+  (ido-everywhere 1)
+					;(flx-ido-mode 1)
+  ;; disable ido faces to see flx highlights.
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-faces nil)
+  (setq gc-cons-threshold 20000000))
 ;;********************* Transpose frame  *****************************************
-(require 'transpose-frame)
-(global-set-key (kbd "s-r") 'rotate-frame-clockwise)
-(global-set-key (kbd "M-'") 'auto-complete)
+(progn
+  (require 'transpose-frame)
+  (global-set-key (kbd "s-r") 'rotate-frame-clockwise)
+  (global-set-key (kbd "M-'") 'auto-complete))
 ;;********************* End transpose frame *****************************************
 ;; ;************************* Speedbar Related customization ******************
-(require 'sr-speedbar)
-(global-set-key (kbd "C-0") 'sr-speedbar-toggle)
-(global-set-key (kbd "C-9") 'sr-speedbar-select-window)
+(progn
+  (require 'sr-speedbar)
+  (global-set-key (kbd "C-0") 'sr-speedbar-toggle)
+  (global-set-key (kbd "C-9") 'sr-speedbar-select-window))
 ;;****************************** Move lines up and down **********************
 
 ;;********************* Shortcuts *******************************************
@@ -157,14 +161,6 @@
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
 ;******************** End Multiple Cursors ************************************
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; ;****************************** AC and YASnippet ***************
 (progn
   (require 'auto-complete)
@@ -178,48 +174,49 @@
     (yas-global-mode 1)))
 
 ;; ;************************* Web Mode ***********************************
-(require 'web-mode)
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'ac-sources 'ac-source-html-tag)
-(add-to-list 'ac-sources 'ac-source-html-attribute)
-(add-to-list 'web-mode-ac-sources-alist
-             '(("css" . (ac-source-css-property))
-	       ("html" . (ac-source-html-tag
-			  ac-source-words-in-buffer
-			  ac-source-abbrev
-			  ac-source-html-attribute))))
-(add-to-list 'auto-mode-alist '("\\.html*\\'" . web-mode))
-;; adjust indents for web-mode to 2 spaces
-(defun my-web-mode-hook ()
-  "Hooks for Web mode. Adjust indents"
+(progn
+  (require 'web-mode)
+  (require 'js2-mode)
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'ac-sources 'ac-source-html-tag)
+  (add-to-list 'ac-sources 'ac-source-html-attribute)
+  (add-to-list 'web-mode-ac-sources-alist
+	       '(("css" . (ac-source-css-property))
+		 ("html" . (ac-source-html-tag
+			    ac-source-words-in-buffer
+			    ac-source-abbrev
+			    ac-source-html-attribute))))
+  (add-to-list 'auto-mode-alist '("\\.html*\\'" . web-mode))
+  ;; adjust indents for web-mode to 2 spaces
+  (defun my-web-mode-hook ()
+    "Hooks for Web mode. Adjust indents"
   ;;; http://web-mode.org/
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-(add-hook 'web-mode-hook  'my-web-mode-hook)
-(setq standard-indent 2)
-;; ;;*********** JS2 and tern *****************************
-(eval-after-load 'tern
-   '(progn
-      (require 'tern-auto-complete)
-      (tern-ac-setup)))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-(add-hook 'js2-mode-hook 'tern-mode)
-(add-hook 'js2-mode-hook 'ac-js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(if (eq system-type 'windows-nt)
-    (setq tern-command '("node" "C:\\Users\\El\\AppData\\Roaming\\npm\\node_modules\\tern\\bin\\tern")))
-(setq js-indent-level 2)
-(setq js2-basic-offset 2)
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2))
+  (add-hook 'web-mode-hook  'my-web-mode-hook)
+  (setq standard-indent 2)
+  ;; ;;*********** JS2 and tern *****************************
+  (eval-after-load 'tern
+    '(progn
+       (require 'tern-auto-complete)
+       (tern-ac-setup)))
+  (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+  (add-hook 'js2-mode-hook 'tern-mode)
+  (add-hook 'js2-mode-hook 'ac-js2-mode)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+  (if (eq system-type 'windows-nt)
+      (setq tern-command '("node" "C:\\Users\\El\\AppData\\Roaming\\npm\\node_modules\\tern\\bin\\tern")))
+  (setq js-indent-level 2)
+  (setq js2-basic-offset 2))
 ;; ***************************** Flycheck stuff for js *****************************
 ;; http://www.flycheck.org/manual/latest/index.html
 (progn
@@ -246,9 +243,10 @@
 )
 
 ;; ***************************** Racket MOde stuff *****************************
-(require 'racket-mode)
-(add-to-list 'auto-mode-alist '("\\.rkt\\'" . racket-mode))
-(add-hook 'racket-mode-hook 'auto-complete-mode)
+(progn
+  (require 'racket-mode)
+  (add-to-list 'auto-mode-alist '("\\.rkt\\'" . racket-mode))
+  (add-hook 'racket-mode-hook 'auto-complete-mode))
 ;; ;******************** Config ggtags for C++  ***********************************
 ;; C++ stuff from https://github.com/tuhdo/emacs-c-ide-demo/blob/master/init.el
 
@@ -261,112 +259,114 @@
 ;;  helm-gtags-prefix-key "\C-cg"
 ;;  helm-gtags-suggested-key-mapping t
 ;;  )
-(require 'helm-gtags)
-;; ;; Enable helm-gtags-mode
-;; (add-hook 'dired-mode-hook 'helm-gtags-mode)
-;; (add-hook 'eshell-mode-hook 'helm-gtags-mode)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-;; (add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-;; (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-;; (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-;; (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-;; (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
 (progn
-  ;; (add-to-list 'load-path "~/.emacs.d/custom")
-  ;; (require 'setup-helm)
-  ;; (require 'setup-helm-gtags)
-  ;; (require 'setup-ggtags)
-  ;; (require 'setup-cedet)
-  ;; (require 'setup-editing)
+  (require 'helm-gtags)
+  ;; ;; Enable helm-gtags-mode
+  ;; (add-hook 'dired-mode-hook 'helm-gtags-mode)
+  ;; (add-hook 'eshell-mode-hook 'helm-gtags-mode)
+  (add-hook 'c-mode-hook 'helm-gtags-mode)
+  (add-hook 'c++-mode-hook 'helm-gtags-mode)
+  ;; (add-hook 'asm-mode-hook 'helm-gtags-mode)
 
-  ;; (windmove-default-keybindings)
-
-  ;; function-args
+  ;; (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+  ;; (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+  ;; (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  ;; (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
   (progn
-    (require 'function-args)
-    (fa-config-default)
-    (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-    (define-key c++-mode-map  [(control tab)] 'moo-complete)
-    (define-key c++-mode-map (kbd "M-j") 'moo-jump-local)
-    )
+    ;; (add-to-list 'load-path "~/.emacs.d/custom")
+    ;; (require 'setup-helm)
+    ;; (require 'setup-helm-gtags)
+    ;; (require 'setup-ggtags)
+    ;; (require 'setup-cedet)
+    ;; (require 'setup-editing)
 
-  ;; (define-key c-mode-map  [(tab)] 'company-complete)
-  ;; (define-key c++-mode-map  [(tab)] 'company-complete)
+    ;; (windmove-default-keybindings)
 
-  ;; ;; company
-  ;; (require 'company)
-  ;; ;; (add-hook 'after-init-hook 'global-company-mode)
-  ;; ;; (delete 'company-semantic company-backends)
-  ;; ;; (define-key c-mode-map  [(control tab)] 'company-complete)
-  ;; ;; (define-key c++-mode-map  [(control tab)] 'company-complete)
+    ;; function-args
+    (progn
+      (require 'function-args)
+      (fa-config-default)
+      (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+      (define-key c++-mode-map  [(control tab)] 'moo-complete)
+      (define-key c++-mode-map (kbd "M-j") 'moo-jump-local)
+      )
 
-  ;; ;; company-c-headers
-  ;; (add-to-list 'company-backends 'company-c-headers)
+    ;; (define-key c-mode-map  [(tab)] 'company-complete)
+    ;; (define-key c++-mode-map  [(tab)] 'company-complete)
 
-  ;; ;; hs-minor-mode for folding source code
-  ;; (add-hook 'c-mode-common-hook 'hs-minor-mode)
+    ;; ;; company
+    ;; (require 'company)
+    ;; ;; (add-hook 'after-init-hook 'global-company-mode)
+    ;; ;; (delete 'company-semantic company-backends)
+    ;; ;; (define-key c-mode-map  [(control tab)] 'company-complete)
+    ;; ;; (define-key c++-mode-map  [(control tab)] 'company-complete)
 
-  ;; Available C style:
-  ;; “gnu”: The default style for GNU projects
-  ;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
-  ;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
-  ;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
-  ;; “stroustrup”: What Stroustrup, the author of C++ used in his book
-  ;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
-  ;; “linux”: What the Linux developers use for kernel development
-  ;; “python”: What Python developers use for extension modules
-  ;; “java”: The default style for java-mode (see below)
-  ;; “user”: When you want to define your own style
-  ;; (setq
-  ;;  c-default-style "linux" ;; set style to "linux"
-  ;;  )
+    ;; ;; company-c-headers
+    ;; (add-to-list 'company-backends 'company-c-headers)
 
-  ;; activate whitespace-mode to view all whitespace characters
-  (global-set-key (kbd "C-c w") 'whitespace-mode)
+    ;; ;; hs-minor-mode for folding source code
+    ;; (add-hook 'c-mode-common-hook 'hs-minor-mode)
 
-  ;; show unncessary whitespace that can mess up your diff
-  (add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+    ;; Available C style:
+    ;; “gnu”: The default style for GNU projects
+    ;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
+    ;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
+    ;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
+    ;; “stroustrup”: What Stroustrup, the author of C++ used in his book
+    ;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
+    ;; “linux”: What the Linux developers use for kernel development
+    ;; “python”: What Python developers use for extension modules
+    ;; “java”: The default style for java-mode (see below)
+    ;; “user”: When you want to define your own style
+    ;; (setq
+    ;;  c-default-style "linux" ;; set style to "linux"
+    ;;  )
 
-  ;; Compilation
-  (global-set-key (kbd "<f5>") (lambda ()
-                                 (interactive)
-                                 (setq-local compilation-read-command nil)
-                                 (call-interactively 'compile)))
+    ;; activate whitespace-mode to view all whitespace characters
+    (global-set-key (kbd "C-c w") 'whitespace-mode)
 
-  ;; setup GDB
-  (setq
-   ;; use gdb-many-windows by default
-   gdb-many-windows t
+    ;; show unncessary whitespace that can mess up your diff
+    (add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 
-   ;; Non-nil means display source file containing the main routine at startup
-   gdb-show-main t
-   )
+    ;; Compilation
+    (global-set-key (kbd "<f5>") (lambda ()
+				   (interactive)
+				   (setq-local compilation-read-command nil)
+				   (call-interactively 'compile)))
 
-  ;; Package: projejctile
-  (require 'projectile)
-  (projectile-global-mode)
-  (setq projectile-enable-caching t)
+    ;; setup GDB
+    (setq
+     ;; use gdb-many-windows by default
+     gdb-many-windows t
 
-  (require 'helm-projectile)
-  (helm-projectile-on)
-  (setq projectile-completion-system 'helm)
-  (setq projectile-indexing-method 'alien)
-  (global-set-key (kbd "M-p s") 'helm-projectile-switch-project)
-  (global-set-key (kbd "M-p b") 'helm-projectile-switch-to-buffer)
-  ;; Package zygospore - For undoing delete other windows - This is awesome
-  (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows))
+     ;; Non-nil means display source file containing the main routine at startup
+     gdb-show-main t
+     )
+
+    ;; Package: projejctile
+    (require 'projectile)
+    (projectile-global-mode)
+    (setq projectile-enable-caching t)
+
+    (require 'helm-projectile)
+    (helm-projectile-on)
+    (setq projectile-completion-system 'helm)
+    (setq projectile-indexing-method 'alien)
+    (global-set-key (kbd "M-p s") 'helm-projectile-switch-project)
+    (global-set-key (kbd "M-p b") 'helm-projectile-switch-to-buffer)
+    ;; Package zygospore - For undoing delete other windows - This is awesome
+    (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)))
 ;; ;******************** End ggtags ************************************
 ;; ******************* Python suff ***********************************
 ;; Python mode settings
-(require 'python-mode)
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
-(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
-(setq py-electric-colon-active t)
+(progn
+  (require 'python-mode)
+  (add-hook 'python-mode-hook 'jedi:setup)
+  (setq jedi:complete-on-dot t)
+  (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+  (setq py-electric-colon-active t))
 ;; (add-hook 'python-mode-hook 'autopair-mode)(add-hook 'python-mode-hook 'auto-complete-mode)
 ;; ******************* End Python suff ***********************************
 ;; *********************************** Ruby ***********************************
@@ -377,7 +377,6 @@
   (add-hook 'ruby-mode-hook 'ri-bind-key)
   )
 ;; *********************************** End Ruby stuff ***********************************
-(put 'scroll-left 'disabled nil)
 ;; *********************************** Haskell stuff ***********************************
 ;; Standard libraries needed
 (require 'cl-lib)
@@ -395,27 +394,27 @@
 ;; *********************************** End Haskell stuff ***********************************
 ;; *********************************** OCaml stuff ***********************************
 ;; Add opam emacs directory to the load-path
-(progn
-  (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
-  ;; Load merlin-mode
-  ;; (autoload 'merlin-mode "merlin" "Merlin mode" t)
-  (require 'merlin)
-  ;; Start merlin on ocaml files
-  (add-hook 'tuareg-mode-hook 'merlin-mode t)
-  (add-hook 'caml-mode-hook 'merlin-mode t)
-  ;; Enable auto-complete
-  (setq merlin-ac-setup 'easy)
-  (setq merlin-use-auto-complete-mode 'easy)
-  ; Make company aware of merlin
-  (with-eval-after-load 'company
-    (add-to-list 'company-backends 'merlin-company-backend))
-; Enable company on merlin managed buffers
-  (add-hook 'merlin-mode-hook 'company-mode)
-;; Or enable it globally:
-;; (add-hook 'after-init-hook 'global-company-mode)
+;; (progn
+;;   (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+;;   ;; Load merlin-mode
+;;   ;; (autoload 'merlin-mode "merlin" "Merlin mode" t)
+;;   (require 'merlin)
+;;   ;; Start merlin on ocaml files
+;;   (add-hook 'tuareg-mode-hook 'merlin-mode t)
+;;   (add-hook 'caml-mode-hook 'merlin-mode t)
+;;   ;; Enable auto-complete
+;;   (setq merlin-ac-setup 'easy)
+;;   (setq merlin-use-auto-complete-mode 'easy)
+;;   ; Make company aware of merlin
+;;   (with-eval-after-load 'company
+;;     (add-to-list 'company-backends 'merlin-company-backend))
+;; ; Enable company on merlin managed buffers
+;;   (add-hook 'merlin-mode-hook 'company-mode)
+;; ;; Or enable it globally:
+;; ;; (add-hook 'after-init-hook 'global-company-mode)
 
-  ;; Use opam switch to lookup ocamlmerlin binary
-  (setq merlin-command 'opam))
+;;   ;; Use opam switch to lookup ocamlmerlin binary
+;;   (setq merlin-command 'opam))
 ;;********************************** End OCaml stuff **********************************
 ;; ********************************** Java stuff **********************************
 (put 'upcase-region 'disabled nil)
@@ -426,3 +425,4 @@
 ;; (use-package ensime
 ;;   :commands ensime ensime-mode)
 ;; (add-hook 'scala-mode-hook 'ensime-mode)
+(put 'scroll-left 'disabled nil)
